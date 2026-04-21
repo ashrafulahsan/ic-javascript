c67a988435082ea889ed4009627927ed4ccba0f5
ashrafulahsan <ahsanashraful@email.com>
2026-04-21 21:50:03 +0600

    Datatype

---
+++ datatype.js
@@ -0,0 +1,11 @@
+let nl = null // here nl type is object
+let n = undefined // here type of n is undefined
+let e = ''; // here type of e is string
+let v; // here type of n is undefined
+let a = ['a', 'b', 'c'] // here type of a is object
+
+console.log(typeof nl) // output: object
+console.log(typeof n) // output: undefined
+console.log(typeof v) // output: undefined
+console.log(typeof a) // output: object
+console.log(typeof e) // output: string
